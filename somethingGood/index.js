5c12274e8b05c52706ab15ad5f96a31ec226b005
jimmyboy112 <guanj37@gmail.com>
2025-02-26 09:54:38 -0800

    Add files via upload

---
+++ somethingGood/index.js
@@ -0,0 +1,22 @@
+document.querySelector('#add').addEventListener('click', function(){
+    const a = Number(document.querySelector('#num1').value);
+    const b = Number(document.querySelector('#num2').value);
+    document.querySelector('#result').textContent = a+b;
+});
+
+document.querySelector('#subtract').addEventListener('click', function(){
+    const a = Number(document.querySelector('#num1').value);
+    const b = Number(document.querySelector('#num2').value);
+    document.querySelector('#result').textContent = a-b;
+});
+
+document.querySelector('#divide').addEventListener('click', function(){
+    const a = Number(document.querySelector('#num1').value);
+    const b = Number(document.querySelector('#num2').value);
+    document.querySelector('#result').textContent = a/b;
+});
+document.querySelector('#multiple').addEventListener('click', function(){
+    const a = Number(document.querySelector('#num1').value);
+    const b = Number(document.querySelector('#num2').value);
+    document.querySelector('#result').textContent = a*b;
+});
